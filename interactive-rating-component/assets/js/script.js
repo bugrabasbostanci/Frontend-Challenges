cf0e2497527f71cff68e4226a752b1d408335ebc
bb0143sbw <bb0143sbw+github@gmail.com>
2023-11-24 22:20:21 +0300

    refactor: submitted card codes updated

--- interactive-rating-component/assets/js/script.js
+++ interactive-rating-component/assets/js/script.js
@@ -1,7 +1,7 @@
 const cardRateBtns = document.querySelector(".card__rate-buttons");
 const submitBtn = document.querySelector(".submit-button");
-const cardEl = document.querySelector(".card");
-
+const submittedCardEl = document.querySelector(".submitted-card");
+const submitMessage = document.querySelector(".submitted-card__message");
 let rateValue;
 
 cardRateBtns.addEventListener("click", function (e) {
@@ -10,24 +10,8 @@ cardRateBtns.addEventListener("click", function (e) {
   rateValue = target.textContent;
 
   submitBtn.addEventListener("click", function () {
-    cardEl.classList.add("submitted");
-    const html = `
-      <img
-        src="./assets/images/illustration-thank-you.svg"
-        alt="thank you image"
-        class="submitted-card__image"/>
-    
-        <div class="submitted-card__message">
-        You selected ${rateValue} out of 5
-        </div>
-        <section class="submitted-card__content flex-group">
-            <h1 class="submitted-card__title">Thank you!</h1>
-            <p class="submitted-card__text">
-            We appreciate you taking the time to give a rating. If you ever need
-            more support, don’t hesitate to get in touch!
-            </p>
-        </section>
-      `;
-    cardEl.innerHTML = html;
+    submittedCardEl.previousElementSibling.classList.add("hidden");
+    submittedCardEl.classList.remove("hidden");
+    submitMessage.textContent = `You selected ${rateValue} out of 5`;
   });
 });
