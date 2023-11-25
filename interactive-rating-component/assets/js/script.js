d13aaedc3a7350e8f51121e7853c2b0a4a762f20
bb0143sbw <bb0143sbw+github@gmail.com>
2023-11-26 02:55:24 +0300

    refactor: html and css rewrited, js updated

--- interactive-rating-component/assets/js/script.js
+++ interactive-rating-component/assets/js/script.js
@@ -1,17 +1,38 @@
-const cardRateBtns = document.querySelector(".card__rate-buttons");
-const submitBtn = document.querySelector(".submit-button");
-const submittedCardEl = document.querySelector(".submitted-card");
-const submitMessage = document.querySelector(".submitted-card__message");
-let rateValue;
+const formEl = document.querySelector(".form");
 
-cardRateBtns.addEventListener("click", function (e) {
+formEl.addEventListener("submit", function (e) {
+  // prevent the page reload
   e.preventDefault();
-  const target = e.target;
-  rateValue = target.textContent;
 
-  submitBtn.addEventListener("click", function () {
-    submittedCardEl.previousElementSibling.classList.add("hidden");
-    submittedCardEl.classList.remove("hidden");
-    submitMessage.textContent = `You selected ${rateValue} out of 5`;
-  });
+  // get rating value
+  const selectedRating = formEl.rating.value;
+
+  // guard clause
+  if (!selectedRating) return;
+
+  // clear formEl innerHTML
+  formEl.innerHTML = "";
+
+  const stateEl = `
+  <article class="state">
+        <img
+          src="./assets/images/illustration-thank-you.svg"
+          class="state__image"
+          alt="state thank you image"
+          title="thank you"
+          role="presentation"
+        />
+        <p class="state__message">You selected ${selectedRating} out of 5</p>
+        <section class="state__description flex-group">
+          <h1 class="state__title">Thank you!</h1>
+          <p class="state__text">
+            We appreciate you taking the time to give a rating. If you ever need
+            more support, don’t hesitate to get in touch!
+          </p>
+        </section>
+      </article>
+  `;
+
+  // add stateEl into main
+  formEl.parentElement.innerHTML = stateEl;
 });
